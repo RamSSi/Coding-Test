4c9b6d599439297747b89d3590ad045b072ebef7
RamSSi <001229aram@gmail.com>
2022-09-02 08:09:53 +0900

    가장 큰 수 done

--- inflearn/03_이분탐색&그리디/10-역수열.py
+++ inflearn/03_이분탐색&그리디/10-역수열.py
@@ -5,9 +5,24 @@ def Count():
     print()
 
 N = int(input())
-nlist = list(map(int, input().split()))
+rev = list(map(int, input().split()))
+ori = [0] * N
 
-i = 0
-while i < N:
-    if i == 0:
-        
+for i in range(1, N+1):
+    for j in range(N):
+        if rev[i] == 0 and ori[j] == 0:
+            ori[j] = i + 1
+            break
+        elif ori[j] == 0:
+            rev[i] -= 1
+print(*ori, sep=' ')
+
+# for n in range(1, N+1):
+#     cnt = 0
+#     for i in range(N):
+#         if ori[i] == 0:
+#             cnt += 1
+#         if cnt > rev[n - 1]:
+#             ori[i] = n
+#             break
+# print(*ori, sep=' ')
